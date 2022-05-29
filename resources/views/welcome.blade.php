c2a59fee96ea70dfc5f0017899b748d226d62247
Thiago Vieira <tmaciel@digital-works.com>
2022-05-29 21:26:39 +0100

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML>
+<!--
+	Story by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Story by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper" class="divided">
+
+				<!-- One -->
+					<section class="banner style1 orient-left content-align-left image-position-right fullscreen onload-image-fade-in onload-content-fade-right">
+						<div class="content">
+							<h1>Doce Gregorini</h1>
+							<p class="major">Bolos decorados cobertos com Buttercream</p>
+							<ul class="actions stacked">
+								<li><a href="tel:+351 913 125 136" class="button big wide smooth-scroll-middle">Liga-me!</a></li>
+							</ul>
+						</div>
+						<div class="image">
+							<img src=".\assets\images\doce.png" alt="" />
+						</div>
+					</section>
+
+				<!-- Footer -->
+					<footer class="wrapper style1 align-center">
+						<div class="inner">
+							<ul class="icons">
+								<li><a href="https://www.facebook.com/docegregorini" target="_blank"  class="icon brands style2 fa-facebook-f"><span class="label">Facebook</span></a></li>
+								<li><a href="https://www.instagram.com/docegregorini/" target="_blank" class="icon brands style2 fa-instagram"><span class="label">Instagram</span></a></li>
+								<li><a  href="mailto: contacto@docegregorini.pt" target="_blank" class="icon style2 fa-envelope"><span class="label">Email</span></a></li>
+							</ul>
+							<p>&copy; Developed by: <a href="#">StarFish</a>.</p>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
